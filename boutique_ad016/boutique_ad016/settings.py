8c924f998f9843420c325b4dd7d88378017498ca
JModi16 <k5rac70j@students.codeinstitute.net>
2025-09-17 12:50:30 +0100

    removed installed apps in settings.py

--- boutique_ad016/boutique_ad016/settings.py
+++ boutique_ad016/boutique_ad016/settings.py
@@ -38,11 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'products',
-    'bag',
-    'checkout',
-    'profiles',
 ]
+   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
