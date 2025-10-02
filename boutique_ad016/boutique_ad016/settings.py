1c5105ff38c20b7f00d2918e3b2ff14b39480bcf
JModi16 <k5rac70j@students.codeinstitute.net>
2025-10-02 11:29:39 +0100

    removed # from html files

--- boutique_ad016/boutique_ad016/settings.py
+++ boutique_ad016/boutique_ad016/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
     'allauth',                      # <-- Required for allauth
     'allauth.account',              # <-- Required for allauth
     'allauth.socialaccount',        # <-- Required for allauth
-    # 'products',
+    'products',
 ]
    
 
@@ -125,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
