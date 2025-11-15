79871bb37fdd9a2fcc361bd61fce43e1723cf7eb
JModi16 <k5rac70j@students.codeinstitute.net>
2025-11-15 15:14:54 +0000

    adjusted webhooks.py and settings.py

--- boutique_ad016/boutique_ad016/settings.py
+++ boutique_ad016/boutique_ad016/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-bf_nx7m0sd(kbofoqv1g*ba))$ux)2(ju#mr-kr5#21@5iql_d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1' ,  # vs code preview
+    'localhost', # listen for stripe webhooks
+
+]
 
 
 # Application definition
