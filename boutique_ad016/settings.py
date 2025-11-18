d77ee8ec25711d4ca5bc1ee6220cf213d9c80b05
JModi16 <k5rac70j@students.codeinstitute.net>
2025-11-18 14:44:06 +0000

    adjusted settings.py path and moved into correct project folder

--- boutique_ad016/settings.py
+++ boutique_ad016/settings.py
@@ -12,12 +12,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-
-if os.path.exists('env.py'):
-    import env  # Add this line to import environment variables
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Add the parent directory to the path so we can import env.py
+sys.path.insert(0, str(BASE_DIR))
+
+if os.path.exists(os.path.join(BASE_DIR, 'env.py')):
+    import env  # Add this line to import environment variables
 
 
 # Quick-start development settings - unsuitable for production
