7eb2f6e836b921013fde17f30b3e45e1e7cb4c9d
JModi16 <k5rac70j@students.codeinstitute.net>
2025-11-23 23:14:32 +0000

    adjusted dbase settings

--- boutique_ad016/boutique_ad016/settings.py
+++ boutique_ad016/boutique_ad016/settings.py
@@ -25,7 +25,7 @@ if os.path.exists(os.path.join(BASE_DIR, 'env.py')):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bf_nx7m0sd(kbofoqv1g*ba))$ux)2(ju#mr-kr5#21@5iql_d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,6 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1' ,  # vs code preview
     'localhost', # listen for stripe webhooks
+    'boutiqueado16-87312841c5b4.herokuapp.com/', # heroku app
 
 ]
 
@@ -140,6 +141,18 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
