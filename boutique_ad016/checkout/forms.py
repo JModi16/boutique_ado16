52f5bb9573a406055c282962d65bf2d55e928d29
JModi16 <k5rac70j@students.codeinstitute.net>
2025-12-18 13:27:46 +0000

    updated forms.py

--- boutique_ad016/checkout/forms.py
+++ boutique_ad016/checkout/forms.py
@@ -7,14 +7,9 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
-                  'town_or_city', 'postcode', 'country',
-                  'county',)
+                  'town_or_city', 'postcode', 'country', 'county',)
 
     def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
         super().__init__(*args, **kwargs)
         placeholders = {
             'full_name': 'Full Name',
@@ -27,19 +22,19 @@ class OrderForm(forms.ModelForm):
             'county': 'County, State or Locality',
         }
 
-        # Force country choices to a list to avoid BlankChoiceIterator __len__ issue
+        # Force concrete choices to avoid BlankChoiceIterator __len__ issues
         self.fields['country'].choices = list(self.fields['country'].choices)
+        if hasattr(self.fields['country'].widget, 'choices'):
+            self.fields['country'].widget.choices = list(self.fields['country'].widget.choices)
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-                self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+                placeholder = f"{placeholders[field]} *" if self.fields[field].required else placeholders[field]
+                self.fields[field].widget.attrs.update({
+                    'placeholder': placeholder,
+                    'class': 'stripe-style-input'
+                })
             else:
-                # Special styling for country select field
                 self.fields[field].widget.attrs['class'] = 'custom-select d-block w-100'
             self.fields[field].label = False
